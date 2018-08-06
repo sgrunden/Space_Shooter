7d809513009d8d6b20cfbbdaafecd46ff1f2c01d
sgrunden <savannah.grunden@gmail.com>
2018-08-06 14:09:01 -0700

    Unity project

---
+++ Assets/TutorialInfo/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public static GameObject bolt;
+    public Rigidbody rb = bolt.GetComponent<Rigidbody>();
+    public float speed;
+
+    private void Start()
+    {
+        rb.velocity = transform.forward * speed;
+    }
+}
